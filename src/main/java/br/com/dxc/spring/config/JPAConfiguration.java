314b4a120f68dd35958f98254cd69bfcc040f7b5
CSC-SJRP\lsilva49 <lsilva49@dxc.com>
2018-03-12 14:58:38 -0300

    Configuração da JPA e hibernate

--- src/main/java/br/com/dxc/spring/config/JPAConfiguration.java
+++ src/main/java/br/com/dxc/spring/config/JPAConfiguration.java
@@ -2,12 +2,16 @@ package br.com.dxc.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import java.util.Properties;
 
+@EnableTransactionManagement //habilita o spring a trabalhar com transações
 public class JPAConfiguration {
 
     @Bean
@@ -19,11 +23,11 @@ public class JPAConfiguration {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setUsername("root");
         dataSource.setPassword("");
-        dataSource.setUrl("jdbc:mysql//localhost:3306/casadocodigo");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo");
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 
         Properties properties = new Properties();
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect"); //Configura as propriedades do hibernate (dialeto que o hibernate comunicará com o BD)
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");//Configura as propriedades do hibernate (dialeto que o hibernate comunicará com o BD)
         properties.setProperty("hibernate.show_sql", "true"); //Permite ver o sql gerado pelo hibernate
         properties.setProperty("hibernate.hbm2ddl.auto", "update"); //Toda vez que o modelo for atualizado, o hibernate gerará/atualizará o banco
         factoryBean.setJpaProperties(properties);
@@ -31,5 +35,9 @@ public class JPAConfiguration {
         factoryBean.setPackagesToScan("br.com.dxc.spring.models"); //configura onde o spring procurará as entidades do banco
 
         return factoryBean;
+    }
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf){
+        return new JpaTransactionManager(emf); //Associa o JPATrascationManager com o EntityManager
     }
 }
--- src/main/java/br/com/dxc/spring/daos/ProdutoDAO.java
+++ src/main/java/br/com/dxc/spring/daos/ProdutoDAO.java
@@ -2,11 +2,13 @@ package br.com.dxc.spring.daos;
 
 import br.com.dxc.spring.model.Produto;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Repository
+@Transactional
 public class ProdutoDAO {
 
     @PersistenceContext // Faz com que o spring injete o EntityManager
