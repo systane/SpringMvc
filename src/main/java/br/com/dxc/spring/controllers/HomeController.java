18cbbe47ed3cc19129b21bd382b770a9956c901d
Luis Fernando <lsilva49@dxc.com>
2018-05-08 14:34:27 -0300

    correção configuração de produção

--- src/main/java/br/com/dxc/spring/controllers/HomeController.java
+++ src/main/java/br/com/dxc/spring/controllers/HomeController.java
@@ -44,7 +44,7 @@ public class HomeController {
         usuario.setNome("Admin");
         usuario.setEmail("admin@dxc.com.br");
         usuario.setSenha("$2a$10$lt7pS7Kxxe5JfP.vjLNSyOXP11eHgh7RoPxo5fvvbMCZkCUss2DGu"); //123456
-        usuario.setRoles(Arrays.asList(new Role("ADMIN")));
+        usuario.setRoles(Arrays.asList(new Role("ROLE_ADMIN")));
 
         usuarioDao.gravar(usuario);
 
